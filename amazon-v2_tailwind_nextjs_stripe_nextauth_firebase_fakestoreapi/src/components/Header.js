6fed71fe0d946c1f312d68696284409d8a841129
Ht <htcvhs971@gmail.com>
2022-11-28 00:58:13 +0100

    Header set up Top Nav: search

--- amazon-v2_tailwind_nextjs_stripe_nextauth_firebase_fakestoreapi/src/components/Header.js
+++ amazon-v2_tailwind_nextjs_stripe_nextauth_firebase_fakestoreapi/src/components/Header.js
@@ -1,14 +1,27 @@
 import Image from 'next/image';
+import { MenuIcon, SearchIcon, ShoppingCartIcon, MagnifyingGlass } from '@heroicons/react/outline';
 
 function Header() {
 	return (
 		<header>
 			{/* Top Nav */}
 			<div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+				{/* Logo */}
 				<div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
 					<Image src="https://links.papareact.com/f90" width={50} height={4} objectFit="contain" className="cursor-pointer" />
 				</div>
+
+				{/* Search */}
+				<div className="bg-yellow-400 hover:bg-yellow-400 hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer">
+					<input type="text" className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" />
+					<SearchIcon className="h-12 p-4" />
+				</div>
+
+				{/* Right */}
 			</div>
+
+			{/* Bottom Nav */}
+			<div></div>
 		</header>
 	);
 }
