be7bb18048e1f33d5fcc3fe1e2ff6eb3c8b8dd51
Ht <htcvhs971@gmail.com>
2022-11-27 20:01:26 +0100

    Header set up: logo

--- amazon-v2_tailwind_nextjs_stripe_nextauth_firebase_fakestoreapi/src/components/Header.js
+++ amazon-v2_tailwind_nextjs_stripe_nextauth_firebase_fakestoreapi/src/components/Header.js
@@ -1,1 +1,16 @@
-rfce;
+import Image from 'next/image';
+
+function Header() {
+	return (
+		<header>
+			{/* Top Nav */}
+			<div className="flex items-center bg-amazon_blue">
+				<div>
+					<Image src="https://links.papareact.com/f90" width={150} height={40} />
+				</div>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
