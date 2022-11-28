fa539c61cb30c6b66f9e79eaed2c7856ea295920
Ht <htcvhs971@gmail.com>
2022-11-28 10:26:52 +0100

    Header set up Top Nav: locate & right side

--- amazon-v2_tailwind_nextjs_stripe_nextauth_firebase_fakestoreapi/src/components/Header.js
+++ amazon-v2_tailwind_nextjs_stripe_nextauth_firebase_fakestoreapi/src/components/Header.js
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { MenuIcon, SearchIcon, ShoppingCartIcon, MagnifyingGlass } from '@heroicons/react/outline';
+import { MenuIcon, SearchIcon, ShoppingCartIcon, MagnifyingGlass, LocationMarkerIcon } from '@heroicons/react/outline';
 
 function Header() {
 	return (
@@ -11,6 +11,15 @@ function Header() {
 					<Image src="https://links.papareact.com/f90" width={50} height={4} objectFit="contain" className="cursor-pointer" />
 				</div>
 
+				{/* Locate */}
+				<div className="text-white flex ">
+					<LocationMarkerIcon width={20} height={20} objectFit="contain" />
+					<div className="flex-col items-start text-xs space-x-6 mx-6 whitespace-nowrap">
+						<p>Bonjour</p>
+						<p>Entrez votre adresse</p>
+					</div>
+				</div>
+
 				{/* Search */}
 				<div className="bg-yellow-400 hover:bg-yellow-400 hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer">
 					<input type="text" className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" />
@@ -18,6 +27,20 @@ function Header() {
 				</div>
 
 				{/* Right */}
+				<div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+					<div className="link">
+						<p>Hello TOI</p>
+						<p>Compte et listes</p>
+					</div>
+					<div className="link">
+						<p>Retours</p>
+						<p>et commandes</p>
+					</div>
+					<div className="link">
+						{/*TODO:  Shopping cart */}
+						<p>Basket</p>
+					</div>
+				</div>
 			</div>
 
 			{/* Bottom Nav */}
