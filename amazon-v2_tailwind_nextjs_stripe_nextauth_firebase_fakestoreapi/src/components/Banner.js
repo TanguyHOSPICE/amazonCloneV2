0903325a8c5e43c1596cd9f701dea832bf5067f0
Ht <htcvhs971@gmail.com>
2022-12-02 22:52:13 +0100

    Banner:Set up image carousel

--- amazon-v2_tailwind_nextjs_stripe_nextauth_firebase_fakestoreapi/src/components/Banner.js
+++ amazon-v2_tailwind_nextjs_stripe_nextauth_firebase_fakestoreapi/src/components/Banner.js
@@ -1,17 +1,24 @@
 import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function Banner() {
 	return (
 		<div className="relative">
 			<Carousel autoPlay infiniteLoop showStatus={false} showIndicators={false} showThumbs={false} interval={5000}>
 				<div>
-					<img src="https://links.papareact.com/gi" alt="Banner image" loading="lazy" />
+					<img src="https://m.media-amazon.com/images/I/712ofmU4TFL._SX3000_.jpg" alt="Banner image" loading="lazy" />
 				</div>
 				<div>
-					<img src="https://links.papareact.com/gi" alt="Banner image" loading="lazy" />
+					<img src="https://m.media-amazon.com/images/I/71sXVx3fDHL._SX3000_.jpg" alt="Banner image" loading="lazy" />
 				</div>
 				<div>
-					<img src="https://links.papareact.com/gi" alt="Banner image" loading="lazy" />
+					<img src="https://m.media-amazon.com/images/I/618Cbpt94LL._SX3000_.jpg" alt="Banner image" loading="lazy" />
+				</div>
+				<div>
+					<img src="https://m.media-amazon.com/images/I/71DcBT0CLIL._SX3000_.jpg" alt="Banner image" loading="lazy" />
+				</div>
+				<div>
+					<img src="https://m.media-amazon.com/images/I/71R+gWGp7vL._SX3000_.jpg" alt="Banner image" loading="lazy" />
 				</div>
 			</Carousel>
 		</div>
