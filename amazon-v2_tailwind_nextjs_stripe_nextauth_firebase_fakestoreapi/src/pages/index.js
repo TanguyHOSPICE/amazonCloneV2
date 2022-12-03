39ce3e40d44a13fd0ed0da9dbee8a4171a146e39
Ht <htcvhs971@gmail.com>
2022-12-03 12:03:45 +0100

    ProductFeed:Set up

--- amazon-v2_tailwind_nextjs_stripe_nextauth_firebase_fakestoreapi/src/pages/index.js
+++ amazon-v2_tailwind_nextjs_stripe_nextauth_firebase_fakestoreapi/src/pages/index.js
@@ -1,7 +1,8 @@
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import ProductFeed from '../components/ProductFeed';
 
-export default function Home() {
+export default function Home({ products }) {
 	return (
 		<div className="bg-gray-100">
 			<Header />
@@ -9,7 +10,18 @@ export default function Home() {
 				<Banner />
 
 				{/* ProductFeed */}
+				<ProductFeed />
 			</main>
 		</div>
 	);
+}
+
+export async function getServerSideProps(context) {
+	const products = await fetch('https://fakestoreapi.com/products').then((res) => res.json());
+
+	return {
+		props: {
+			products,
+		},
+	};
 }
