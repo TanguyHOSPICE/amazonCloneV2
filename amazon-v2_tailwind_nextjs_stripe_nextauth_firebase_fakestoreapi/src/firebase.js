392b7bca387c364de9980e0c70906da9cddb3850
Ht <htcvhs971@gmail.com>
2022-12-07 15:53:06 +0100

    NextAuth: Firebase inject

---
+++ amazon-v2_tailwind_nextjs_stripe_nextauth_firebase_fakestoreapi/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyB7hK6doGJDx7RNhH3ZlHgRbZqIGaiTwJk',
+	authDomain: 'ht-clone.firebaseapp.com',
+	projectId: 'ht-clone',
+	storageBucket: 'ht-clone.appspot.com',
+	messagingSenderId: '914316955316',
+	appId: '1:914316955316:web:757f8bf26f6365716cd1e1',
+	measurementId: 'G-F31L8LPP46',
+};
